ee315c80927552ed65e95bf0a390d21de017c203
Ailton Gulmaneli <ailton_gulmaneli@hotmail.com>
2023-05-19 19:10:56 -0300

    Add Crud and initial updates in auth. refatoring index.html

--- contact/forms.py
+++ contact/forms.py
@@ -1,46 +1,26 @@
 
 from django import forms
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import password_validation
 from . import models
 
-class ContactForm(forms.ModelForm):
-    first_name= forms.CharField(
-        widget=forms.TextInput(
+class ContactForm(forms.ModelForm):     
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-             'class':'classe-a classe-b',
-             'placeholder':'Type here third time',  
+                'accept':'image/*',
             }
-        ),
-        label='First Name',
-        help_text=' user help text',
-        
-    )
-    
-    def __init__(self, *args, **kwargs):
-      super().__init__(*args, **kwargs)  
-      
-    #   self.fields ['first_name'].widget.attrs.update({
-    #     'class':'classe-a classe-b',
-    #     'placeholder':'Type here ',
-    #   })
-      
+        )
+    ) 
     class Meta:
         model = models.Contact
         fields = (
-            'first_name','last_name','phone', 'email','description', 'category',
+            'first_name','last_name','phone', 'email','description', 'category','picture',
         )
-        # widgets ={
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class':'classe-a classe-b',
-        #             'placeholder':'Type here'
-        #         }
-        #     )
-        # }
-        
+
     def clean(self):
-        #cleaned_data = self.cleaned_data
         first_name = self.cleaned_data.get('first_name')
         last_name = self.cleaned_data.get('last_name')
 
@@ -50,23 +30,6 @@ class ContactForm(forms.ModelForm):
                 code='invalid'
             )
             self.add_error('last_name', msg)
-            
-        # self.add_error(
-        #     'first_name',
-        #     ValidationError(
-        #         'error message',
-        #         code='invalid'
-        #     )
-        # )
-        
-        # self.add_error(
-        #   'first_name',
-        #   ValidationError(
-        #     'litle Billy',
-        #     code='invalid'
-        #   )
-        # )
-        
         return super().clean()
     
     def clean_first_name(self):
@@ -81,4 +44,119 @@ class ContactForm(forms.ModelForm):
             )
           )
         return first_name
+        
+        
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required =True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required =True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = (
+            'first_name','last_name', 'email', 'username', 'password1', 'password2',
+        )
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('e-mail alread exists', code='invalid')
+            )
+        return email
+    
+class RegisterUpdateForm (forms.ModelForm):
+    first_name= forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='required.',
+        error_messages={
+            'min_length': 'Please, type your name.'
+        }
+    )
+    last_name= forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='required.',
+        error_messages={
+            'min_length': 'Please, type your last name.'
+        }
+    )
+    password1= forms.CharField(
+        label="password1",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete":"new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+    password2= forms.CharField(
+        label="password2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete":"new-password"}),
+        help_text='Use the same password as before',
+        required=False,
+    )
+    class Meta:
+        model = User
+        fields = (
+            'first_name','last_name', 'email', 'username',
+        )
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+        
+        password = cleaned_data.get('password1')
+        
+        if password:
+            user.set_password(password)
+            
+        if commit:
+            user.save()
+        
+        return user
+        
+    def clean(self):
+        password1= self.cleaned_data.get('password1')
+        password2= self.cleaned_data.get('password2')
+        
+        if password1 or password2:
+            if password1 != password2:
+                    self.add_error(
+                        'password2',
+                        ValidationError(' Passwords must be the same.')
+                    )
+        
+        return super().clean()
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email= self.instance.email
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                'email',
+                    ValidationError('e-mail alread exists', code='invalid')
+            )
+        return email
+    
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+        return password1
         
---
+++ contact/views/user_forms.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
+from contact.forms import RegisterForm, RegisterUpdateForm
+
+def register(request):
+    form = RegisterForm()
+    
+    # messages.info(request, ' some message')
+    # messages.success(request, ' some message')
+    # messages.warning(request, ' some message')
+    # messages.error(request, ' some message')
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'user created')
+            return redirect('contact:index')
+    
+    return render(
+        request,
+        'contact/register.html',
+        {
+            'form': form
+        }
+    )
+    
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+        
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+    
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+    form.save()    
+    return redirect('contact:user_update')
+    
+    
+def login_view(request):
+    form = AuthenticationForm(request)
+    
+    if request.method =='POST':
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                user= form.get_user()
+                auth.login(request, user)
+                messages.success(request, 'sucsses login')
+                return redirect('contact:index')
+            messages.error(request, 'Invalid keys')
+        
+    
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+    
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
